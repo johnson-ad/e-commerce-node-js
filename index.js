254e9d9468cf9b4581818a53d47353d04949c6e8
johnson-ad <one007stone@gmail.com>
2023-08-29 14:10:37 +0000

    Connection a la base de donnee

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello")
+const express = require("express")
+const connectDb = require("./config/dbConnection")
+const dotenv = require("dotenv").config()
+
+// Connection wicth  mongoDB
+connectDb()
+
+const app = express()
+app.use(express.json())
+const port = process.env.PORT || 5001
+
+
+//La gestion des error
+//app.use(errorHandler)
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
